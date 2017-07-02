8acc7a61813e38fe5ca3d168a42fd9b77a882226
isLishude <lishude@aliyun.com>
2017-07-02 14:48:23 +0800

    feat: add ans.txt to ans.sql

---
+++ ans.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const os = require('os')
+
+/*
+*  小蚁客户端生成地址，导出为txt文件
+*  重命名为ans.txt放入项目根目录  
+*  运行node ans.js即可
+*  生成的sql放在了outputs/ans.sql中
+*/
+let i = fs.readFileSync('address.txt', 'utf-8');
+if (i.match('\r\n')) {
+    var arrAns = i.split('\r\n');
+} else {
+    var arrAns = i.split('\n');
+}
+
+var length = arrAns.length;
+var sql = '';
+
+for (let i = 0; i < length; i++) {
+    var tmp = 'insert into addresses (user_id,address,type,balance,created_at,updated_at) values (' +
+        i + ',"' + arrAns[i] + '","' + 'ans' + '",' + 0 + ',' + null + ',' + null + ')' + ';\n';
+    sql += tmp;
+}
+
+fs.writeFile('outputs/ans.sql',sql,'utf-8',(err) => {
+    if(err){
+        console.error(err);
+    }else{
+        console.info('Writed succeed!')
+    }
+})
